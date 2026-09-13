8528366750e212f4f4623e0f320cd2e51c0db415
Imanshadowww <imanshadowww@atomicmail.io>
2026-09-13 04:04:06 +0330

    Refactor cli command
    Moved socket address before the 'up' command in cli invocation.

--- worker.sh
+++ worker.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 echo "[Worker-Process]: Booting daemon core..."
-# اجرای هسته با فایل تغییر نام یافته و سوکت محلی
 ./daemon --tun=userspace-networking --socks5-server=localhost:1055 --statedir=./ --socket=./app.sock &
 
 sleep 5 
 
 echo "[Worker-Process]: Authenticating node to master cluster..."
-# لاگین به شبکه با متغیر پوششی جدید
-./cli up --authkey="${AUTH_TOKEN}" --hostname=cloud-worker-node --advertise-exit-node --socket=./app.sock &
+# آدرس سوکت به قبل از دستور up منتقل شد
+./cli --socket=./app.sock up --auth-key="${AUTH_TOKEN}" --hostname=cloud-worker-node --advertise-exit-node &
